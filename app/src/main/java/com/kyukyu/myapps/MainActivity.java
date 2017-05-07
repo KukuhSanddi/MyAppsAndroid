2747ac90e772382679766c3e9da6e47d4352848b
KukuhSanddi <kukuhsanddi12@gmail.com>
2017-05-07 14:09:06 +0700

    Second commit

--- app/src/main/java/com/kyukyu/myapps/MainActivity.java
+++ app/src/main/java/com/kyukyu/myapps/MainActivity.java
@@ -2,12 +2,53 @@ package com.kyukyu.myapps;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+        int numberOfCoffee=0;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+        }
+
+        /**
+         * This method is called when the order button is clicked.
+         */
+        public void submitOrder(View view) {
+
+            //numberOfCoffee++;
+//            display(numberOfCoffee);
+            displayPrice(numberOfCoffee*5);
+        }
+
+        /**
+        * This method displays the given price on the screen.
+        */
+        private void displayPrice(int number) {
+            TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+            priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+        }
+
+    /**
+         * This method displays the given quantity value on the screen.
+         */
+        private void display(int number) {
+            TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+            quantityTextView.setText("" + number);
+        }
+
+        private void increment(View view){
+            numberOfCoffee++;
+            display(numberOfCoffee);
+        }
+
+        private void decrement(View view){
+            numberOfCoffee--;
+            display(numberOfCoffee);
+        }
     }
-}
